f43887b932d39f73ffb8fe7a6c9fd30a68b24ad0
szymanskir <ryszard.szymanski@outlook.com>
2019-11-11 22:15:03 +0100

    Make puppeteer regenerate cheatsheet before export

--- scripts/generate-pdf.js
+++ scripts/generate-pdf.js
@@ -1,15 +1,20 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-    await page.emulateMedia('screen');
-    await page.pdf({
-      path: './react.pdf', // path (relative to CWD) to save the PDF to.
-      printBackground: true,// print background colors
-      width: '842px',
-      height: '595px',
-    });
-    await browser.close();
-  })()
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000/');
+  await page.emulateMedia('screen');
+  const ymlInput = await page.$('#ymlInput');
+  const logoInput = await page.$('#logoInput')
+  await ymlInput.uploadFile("example/content.yml")
+  await logoInput.uploadFile("example/logo.png")
+  await page.waitForSelector("#logoInput")
+  await page.pdf({
+    path: './react.pdf', // path (relative to CWD) to save the PDF to.
+    printBackground: true,// print background colors
+    width: '842px',
+    height: '595px',
+  });
+  await browser.close();
+})()
