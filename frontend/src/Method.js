f18316fa2170dbeb8c619eaaaa732df6c1efca25
Zuzanna Magierska <zuzia.magierska@gmail.com>
2019-12-01 17:40:32 +0100

    Add possibility to break line with '\n'

--- frontend/src/Method.js
+++ frontend/src/Method.js
@@ -10,19 +10,22 @@ export default function Method({ method }) {
     return (
         <div className="method">
             <p>
-                <font className="method-name">{method.name}{method.withoutBrackets !== true && '('}</font>
-                    <font className="method-parameter">{method.withoutBrackets !== true && method.params && method.params.map(p => p.name).join(', ')}</font>
-                <font className="method-name">{method.withoutBrackets !== true && ')'}</font>
-            </p>            
+                <span className="method-name">{method.name}{method.withoutBrackets !== true && '('}</span>
+                <span className="method-parameter">{
+                    method.withoutBrackets !== true && method.params &&
+                    method.params.map(p => p.name).join(', ')
+                }</span>
+                <span className="method-name">{method.withoutBrackets !== true && ')'}</span>
+            </p>
             <p className="method-description">{method.description}</p>
-            {method.params && method.params.map((p, i) =>  
-                <Parameter 
+            {method.params && method.params.map((p, i) =>
+                <Parameter
                     key={i}
                     parameter={p}
                 />
             )}
-            {method.examples && method.examples.map((e, i) =>  
-                <Example 
+            {method.examples && method.examples.map((e, i) =>
+                <Example
                     key={i}
                     example={e}
                 />
